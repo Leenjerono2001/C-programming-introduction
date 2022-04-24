cb9d965eb6d115162310e46d7b6067de2375d1be
Leenjerono2001 <102831045+Leenjerono2001@users.noreply.github.com>
2022-04-24 17:19:40 +0300

    Add files via upload

---
+++ program to print my name.c
@@ -0,0 +1,8 @@
+//program to print my name
+#include<stdio.h>
+int main()
+{
+    printf("Maleen serem");
+    return 0;
+}
+    
---
+++ volume of a sphere.c
@@ -0,0 +1,12 @@
+// program to find a volume of a sphere
+#include <stdio.h>
+int main()
+{
+    //variable declaration
+    int r=56;
+    float pie=3.142;
+    double volume=(4.0/3.0)*pie*(r*r*r);
+    printf("volume of a sphere = %f",volume);
+    
+    return 0;
+}
